a2c4a3b6dce730f74755b08b5eaa85eab8275333
Gokul Shanmugasundaram <162133861+Goku0498@users.noreply.github.com>
2024-12-08 18:37:27 +0530

    Completed Task

---
+++ src/Components/Fullstack.jsx
@@ -0,0 +1,23 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/prop-types */
+import React from "react";
+import Card from "./Card";
+
+const Fullstack = ({ data }) => {
+  const fullstack = data.filter((item) => item.head === "Full Stack");
+  return (
+    <div className="container">
+      <div className="row py-5 d-flex justify-content-center">
+        {fullstack.map((item, index) => {
+          return (
+            <>
+              <Card item={item} index={index} />
+            </>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default Fullstack;
